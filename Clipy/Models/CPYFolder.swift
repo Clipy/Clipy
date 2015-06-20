78d211d2351182b2671d6da7ab817c3c45e2e7a4
ShunsukeFurubayashi <f.s.1992.ip@gmail.com>
2015-06-21 03:38:47 +0900

    Add folder and snippet models

---
+++ Clipy/Models/CPYFolder.swift
@@ -0,0 +1,18 @@
+//
+//  CPYFolder.swift
+//  Clipy
+//
+//  Created by 古林俊佑 on 2015/06/21.
+//  Copyright (c) 2015年 Shunsuke Furubayashi. All rights reserved.
+//
+
+import Cocoa
+
+class CPYFolder: RLMObject {
+
+    dynamic var index       = 0
+    dynamic var enable      = true
+    dynamic var title       = ""
+    dynamic var snippets    = RLMArray(objectClassName: CPYSnippet.className())
+    
+}
---
+++ Clipy/Models/CPYSnippet.swift
@@ -0,0 +1,19 @@
+
+//
+//  CPYSnippet.swift
+//  Clipy
+//
+//  Created by 古林俊佑 on 2015/06/21.
+//  Copyright (c) 2015年 Shunsuke Furubayashi. All rights reserved.
+//
+
+import Cocoa
+
+class CPYSnippet: RLMObject {
+    
+    dynamic var index   = 0
+    dynamic var enable  = true
+    dynamic var title   = ""
+    dynamic var content = ""
+
+}
