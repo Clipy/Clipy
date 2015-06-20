9b3a9b717dd6215fa085be116e20806679dfc933
ShunsukeFurubayashi <f.s.1992.ip@gmail.com>
2015-06-21 03:47:01 +0900

    Add manages

---
+++ Clipy/Managers/CPYClipManager.swift
@@ -0,0 +1,13 @@
+//
+//  CPYClipManager.swift
+//  Clipy
+//
+//  Created by 古林俊佑 on 2015/06/21.
+//  Copyright (c) 2015年 Shunsuke Furubayashi. All rights reserved.
+//
+
+import Cocoa
+
+class CPYClipManager: NSObject {
+
+}
---
+++ Clipy/Managers/CPYMenuManager.swift
@@ -0,0 +1,13 @@
+//
+//  CPYMenuManager.swift
+//  Clipy
+//
+//  Created by 古林俊佑 on 2015/06/21.
+//  Copyright (c) 2015年 Shunsuke Furubayashi. All rights reserved.
+//
+
+import Cocoa
+
+class CPYMenuManager: NSObject {
+
+}
