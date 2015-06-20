c09be1e52b78c1200f329f9b6bd033460e5c46c8
ShunsukeFurubayashi <f.s.1992.ip@gmail.com>
2015-06-21 02:23:28 +0900

    Add project file

---
+++ Clipy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Clipy
+//
+//  Created by 古林俊佑 on 2015/06/21.
+//  Copyright (c) 2015年 Shunsuke Furubayashi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
