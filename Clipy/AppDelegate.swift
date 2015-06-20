03484600a6cc7f82bd551aa6a8703f369fa41a83
ShunsukeFurubayashi <f.s.1992.ip@gmail.com>
2015-06-21 04:45:31 +0900

    Extend AppDelegate

--- Clipy/AppDelegate.swift
+++ Clipy/AppDelegate.swift
@@ -9,18 +9,73 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject {
 
 
+    // MARK: - Init
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.initController()
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    private func initController() {
+        self.registerUserDefaultKey()
+        
+        
+    }
+
+    // MARK: - Override Methods
+    override func validateMenuItem(menuItem: NSMenuItem) -> Bool {
+        return true
+    }
+    
+    // MARK: - KVO
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        
+    }
+    
+    // MARK: - Menu Actions
+    internal func showPreferenceWindow() {
+        
+    }
+    
+    internal func showSnippetEditorWindow() {
+        
+    }
+    
+    internal func clearAllHistory() {
+        
+    }
+    
+    internal func selectClipMenuItem() {
+        
+    }
+    
+    internal func selectSnippetMenuItem() {
+        
+    }
+    
+    // MARK: - Register User Default Keys
+    private func registerUserDefaultKey() {
+        
+    }
+   
+
+}
+
+// MARK: - NSApplication Delegate
+extension AppDelegate: NSApplicationDelegate {
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
     }
-
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
+ 
 }
-
