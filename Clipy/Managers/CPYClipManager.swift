8e63e5ccbfe46299aae113fe016e362fa519b6c9
ShunsukeFurubayashi <f.s.1992.ip@gmail.com>
2015-06-29 21:39:36 +0900

    Setting shortcut recorder

--- Clipy/Managers/CPYClipManager.swift
+++ Clipy/Managers/CPYClipManager.swift
@@ -330,8 +330,6 @@ class CPYClipManager: NSObject {
         let typeDict = CPYClipData.availableTypesDictinary()
         if let key = typeDict[type] {
             if let number = self.storeTypes[key] {
-                println(key)
-                println(number)
                 return number.boolValue
             }
         }
