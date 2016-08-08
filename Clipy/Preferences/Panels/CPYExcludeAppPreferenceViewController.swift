a0455fc697db9687196e70cf1a1b9e34fb7357b2
ShunsukeFurubayashi <f.s.1992.ip@gmail.com>
2016-08-08 22:10:34 +0900

    Add exclude app preference tab

---
+++ Clipy/Preferences/Panels/CPYExcludeAppPreferenceViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CPYExcludeAppPreferenceViewController.swift
+//  Clipy
+//
+//  Created by 古林俊佑 on 2016/08/08.
+//  Copyright © 2016年 Shunsuke Furubayashi. All rights reserved.
+//
+
+import Cocoa
+
+class CPYExcludeAppPreferenceViewController: NSViewController {
+    
+}
