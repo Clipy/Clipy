4cc93a2b35a9a25a874a69ea75a7326be192f2a8
Vincenzo Favara <vin.favara@gmail.com>
2017-04-08 11:57:28 +0100

    Added Snippets multi management

---
+++ Clipy/Sources/Extensions/Realm+Object.swift
@@ -0,0 +1,34 @@
+//
+//  Realm+Object.swift
+//  Clipy
+//
+//  Created by Vincenzo Favara on 25/03/2017.
+//  Copyright © 2017 Shunsuke Furubayashi. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+extension Object {
+    func toDictionary() -> NSDictionary {
+        let properties = self.objectSchema.properties.map { $0.name }
+        let dictionary = self.dictionaryWithValues(forKeys: properties)
+        let mutabledic = NSMutableDictionary()
+        mutabledic.setValuesForKeys(dictionary)
+        
+        for prop in self.objectSchema.properties as [Property]! {
+            // find lists
+            if let nestedObject = self[prop.name] as? Object {
+                mutabledic.setValue(nestedObject.toDictionary(), forKey: prop.name)
+            } else if let nestedListObject = self[prop.name] as? ListBase {
+                var objects = [AnyObject]()
+                for index in 0..<nestedListObject._rlmArray.count  {
+                    let object = nestedListObject._rlmArray[index] as AnyObject
+                    objects.append(object.toDictionary())
+                }
+                mutabledic.setObject(objects, forKey: prop.name as NSCopying)
+            }
+        }
+        return mutabledic
+    }
+}
