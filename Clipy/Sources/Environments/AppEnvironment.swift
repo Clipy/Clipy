882e4730b9ea0fa8a08b8e52cf41e2000a18834f
Econa77 <s.f.1992.ip@gmail.com>
2017-08-10 01:12:40 +0900

    Introduce environments

---
+++ Clipy/Sources/Environments/AppEnvironment.swift
@@ -0,0 +1,67 @@
+//
+//  AppEnvironment.swift
+//  Clipy
+//
+//  Created by 古林俊佑 on 2017/08/10.
+//  Copyright © 2017年 Shunsuke Furubayashi. All rights reserved.
+//
+
+import Foundation
+
+struct AppEnvironment {
+
+    // MARK: - Properties
+    private static var stack = [Environment()]
+
+    static var current: Environment {
+        return stack.last ?? Environment()
+    }
+
+    // MARK: - Stacks
+    static func push(environment: Environment) {
+        stack.append(environment)
+    }
+
+    @discardableResult
+    static func popLast() -> Environment? {
+        return stack.popLast()
+    }
+
+    static func replaceCurrent(environment: Environment) {
+        push(environment: environment)
+        stack.remove(at: stack.count - 2)
+    }
+
+    static func push(clipService: ClipService = current.clipService,
+                     hotKeyService: HotKeyService = current.hotKeyService,
+                     dataCleanService: DataCleanService = current.dataCleanService,
+                     pasteService: PasteService = current.pasteService,
+                     excludeAppService: ExcludeAppService = current.excludeAppService,
+                     menuManager: MenuManager = current.menuManager,
+                     defaults: UserDefaults = current.defaults) {
+        push(environment: Environment(clipService: clipService,
+                                      hotKeyService: hotKeyService,
+                                      dataCleanService: dataCleanService,
+                                      pasteService: pasteService,
+                                      excludeAppService: excludeAppService,
+                                      menuManager: menuManager,
+                                      defaults: defaults))
+    }
+
+    static func replaceCurrent(clipService: ClipService = current.clipService,
+                               hotKeyService: HotKeyService = current.hotKeyService,
+                               dataCleanService: DataCleanService = current.dataCleanService,
+                               pasteService: PasteService = current.pasteService,
+                               excludeAppService: ExcludeAppService = current.excludeAppService,
+                               menuManager: MenuManager = current.menuManager,
+                               defaults: UserDefaults = current.defaults) {
+        replaceCurrent(environment: Environment(clipService: clipService,
+                                                hotKeyService: hotKeyService,
+                                                dataCleanService: dataCleanService,
+                                                pasteService: pasteService,
+                                                excludeAppService: excludeAppService,
+                                                menuManager: menuManager,
+                                                defaults: defaults))
+    }
+
+ }
