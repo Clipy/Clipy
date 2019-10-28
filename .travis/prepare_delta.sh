e40e4d196816951e6ba3b65d8934dee150bacf6c
hjx胡继续 <jixuhu@ctrip.com>
2019-10-29 03:54:24 +0800

    handle download error

--- .travis/prepare_delta.sh
+++ .travis/prepare_delta.sh
@@ -5,4 +5,4 @@ mkdir -p .delta
 #rm -rf .delta/*
 # exract 5 history versions
 versions=$(git tag -l --sort="v:refname" | tail -6 | head -5 | xargs | tr ' ' ',')
-curl -f -L -o .delta/Clipy.app-#1.zip "https://github.com/ian4hu/Clipy/releases/download/{$versions}/Clipy.app.zip"
+curl --retry 3 --connect-timeout 20 -m 300 -f -L -o .delta/Clipy.app-#1.zip "https://github.com/ian4hu/Clipy/releases/download/{$versions}/Clipy.app.zip"
