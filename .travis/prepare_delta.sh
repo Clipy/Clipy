d582e88d9e82398c71fc4511097ad79d1027d871
xuge <jixu.hjx@antfin.com>
2019-10-30 17:08:40 +0800

    1.2.9.beta10

--- .travis/prepare_delta.sh
+++ .travis/prepare_delta.sh
@@ -3,6 +3,7 @@ set -x
 
 mkdir -p .delta
 #rm -rf .delta/*
+tags=$(git ls-remote -t --symref | grep -v '\^' | cut -f 2 | sed s@refs/tags/@@g)
 # exract 5 history versions
-versions=$(git tag -l --sort="v:refname" | tail -6 | head -5 | xargs | tr ' ' ',')
+versions=$(echo "$tags" | tail -6 | head -5 | xargs | tr ' ' ',')
 curl -f -L -o .delta/Clipy.app-#1.zip "https://github.com/ian4hu/Clipy/releases/download/{$versions}/Clipy.app.zip"
